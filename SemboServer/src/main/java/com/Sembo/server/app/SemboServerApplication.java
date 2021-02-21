fb0a341cf22afa30853d92156cb39c47478a2da8
tonisalas <tonisalas@gmail.com>
2021-02-21 08:55:45 +0100

    Server applicartion starter.

---
+++ SemboServer/src/main/java/com/Sembo/server/app/SemboServerApplication.java
@@ -0,0 +1,13 @@
+package com.Sembo.server.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemboServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemboServerApplication.class, args);
+	}
+
+}
